84be46217e4324c31037120222383667ecc4a40d
Joao Pedro Miranda Marques <joaopedromiranda2003@gmail.com>
2026-06-14 18:05:19 -0300

    student: implementa pareamento de syscalls

--- src/student/pairer.c
+++ src/student/pairer.c
@@ -4,6 +4,27 @@ int student_pair_syscall(struct syscall_pairer *pairer,
                          const struct syscall_event *ev,
                          struct syscall_event *out)
 {
+    if (pairer == NULL || ev == NULL || out == NULL){
+        return -1;
+    }
+    if (ev->entering) {
+        if (pairer->has_entry) {
+            return -1; 
+        }
+        pairer->entry = *ev;
+        pairer->has_entry = 1;
+        return 0;
+    }
+    if (!pairer->has_entry){
+        return -1;
+    }
+    
+    *out = pairer->entry;
+    out->entering = 0;
+    out->ret = ev->ret;
+
+    pairer->has_entry = 0; 
+    return 1;
     /*
      * TODO Semana 2:
      *
